94eefeb589ec3b4521613fb29b5b404ba3687927
Pablo Severini <pseverini84@gmail.com>
2020-07-01 01:42:31 +0100

    Se agregan los datos del alumno SEVERINI, Pablo

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s PABLO_SEVERINI = {
+    .apellidos = "SEVERINI",
+    .nombres = "Pablo",
+    .documento = "31.045.507",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &PABLO_SEVERINI,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
