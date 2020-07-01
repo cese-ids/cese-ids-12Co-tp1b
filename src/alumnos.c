6b6ac4b09a1fc5f68af9133b2e49144fe9ee12f7
Pablo Aguirre <aguirre.pablo.1996@gmail.com>
2020-06-30 21:38:32 -0300

    Se agregan los datos del alumno AGUIRRE, Pablo

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s PABLO_AGUIRRE = {
+    .apellidos = "AGUIRRE",
+    .nombres = "Pablo Andres",
+    .documento = "39.698.978",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &PABLO_AGUIRRE,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
